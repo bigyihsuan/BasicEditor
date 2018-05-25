7473ad0670736d7853142bdcc84abb468f787087
bigyihsuan <bigyihsuan@gmail.com>
2018-05-25 09:32:01 -0400

    stuff

--- src/basicEditor/BasicEditorMain.java
+++ src/basicEditor/BasicEditorMain.java
@@ -8,40 +8,82 @@ public class BasicEditorMain {
 	static LineNode pointer = root;
 
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
 		boolean wantToExit = false;
 		boolean hasPrevious = false;
 		LineNode temp = null;
 		LineNode prev = null;
+		TreeSet<Integer> lineNums = new TreeSet<Integer>();
+
 		//main loop
 		System.out.println("READY. INPUT A COMMAND:");
 		do {
 			pointer = root;
 			System.out.print("] ");
 			String in = input.nextLine();
-			
-			//handle attaching code to end of the program
+
+			//handle typing in code
 			if (Character.isDigit(in.charAt(0))) {
-				
 				temp = new LineNode(in);
+
+				//invalid line number (negatives and 0)
 				while (temp.getLineNumber() <= 0) {
 					System.err.println("ERROR: INVALID LINE NUMBER.\n"
-										+ "INPUT CODE WITH POSITIVE LINE NUMBER:");
+							+ "INPUT CODE WITH POSITIVE LINE NUMBER:\n" + "] ");
 					in = input.nextLine();
-					
-					
 				}
-				
+				//delete line
+
+				//adding to end of program
+				//empty program
 				if (!hasPrevious && temp != null) {
+					lineNums.add(temp.getLineNumber());
 					root = temp;
 					prev = root;
 					hasPrevious = true;
 				}
+				//has something there
 				else if (hasPrevious && temp != null){
-					prev.setNext(temp);
-					prev = temp;
-					hasPrevious = true;
+					//handle changing a line
+					if (lineNums.contains(temp.getLineNumber())) {
+						//changing the code
+						LineNode tempPointer = root;
+						while (tempPointer != null) {
+							if (tempPointer.getLineNumber() == temp.getLineNumber()) {
+								//change a code line
+								if (!temp.getCode().equals("")) {
+									tempPointer.setCode(temp.getCode());
+									break;
+								}
+								//delete a line
+								else {
+									if (tempPointer == root) {
+										tempPointer.getNext().setPrevious(null);
+										root = tempPointer.getNext();
+									}
+									else if (tempPointer.getNext() != null) {
+										tempPointer.getPrevious().setNext(tempPointer.getNext());
+										tempPointer.getNext().setPrevious(tempPointer.getPrevious());
+									}
+									else {
+										
+									}
+								}
+							}
+							else {
+								tempPointer = tempPointer.getNext();
+							}
+						}
+					}
+					//add new line
+					else {
+						lineNums.add(temp.getLineNumber());
+						prev.setNext(temp);
+						temp.setPrevious(prev);
+						prev = temp;
+						hasPrevious = true;
+					}
 				}
 			}
 			else if (in.toUpperCase().equals("LIST")) {
@@ -50,7 +92,7 @@ public class BasicEditorMain {
 		} while (!wantToExit);
 
 	}
-	
+
 	public static void list() {
 		if (pointer == null) {
 			System.out.println();
@@ -61,6 +103,7 @@ public class BasicEditorMain {
 				pointer = pointer.getNext();
 			}
 		}
+		pointer = root;
 	}
 
 }
--- src/basicEditor/LineNode.java
+++ src/basicEditor/LineNode.java
@@ -5,6 +5,7 @@ public class LineNode {
 	private int lineNumber;
 	private String code;
 	private LineNode next;
+	private LineNode prev;
 	
 	public LineNode(String input) {
 		parse(input);
@@ -32,11 +33,15 @@ public class LineNode {
 				lnEnd = i;
 				break;
 			}
+			lnEnd = input.length();
 		}
 		
 		lineNumber = Integer.parseInt(input.substring(lnStart, lnEnd));		
-		code = input.substring(lnEnd).toUpperCase();
+		if (input.length() > new Integer(input.substring(lnStart, lnEnd)).toString().length()) {
+			code = input.substring(lnEnd).toUpperCase();
+		}
 		next = null;
+		prev = null;
 	}
 
 	public int getLineNumber() {
@@ -50,6 +55,10 @@ public class LineNode {
 	public LineNode getNext() {
 		return next;
 	}
+	
+	public LineNode getPrevious() {
+		return prev;
+	}
 
 	public void setLineNumber(int ln) {
 		lineNumber = ln;
@@ -61,6 +70,14 @@ public class LineNode {
 
 	public void setNext(LineNode n) {
 		next = n;
+	}
+	
+	public void setPrevious(LineNode p) {
+		prev = p;
+	}
+	
+	public boolean hasNext() {
+		return next != null;
 	}
 	
 	
