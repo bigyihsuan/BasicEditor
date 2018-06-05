42177b08f82c4aa4543f2de1f2140e1797ccdb35
bigyihsuan <bigyihsuan@gmail.com>
2018-06-05 08:53:00 -0400

    type system

--- src/basicEditor/LineNode.java
+++ src/basicEditor/LineNode.java
@@ -1,26 +1,27 @@
 package basicEditor;
 
 public class LineNode {
-	
+
 	private int lineNumber;
 	private String code;
 	private LineNode prev;
 	private LineNode next;
-	private boolean isAssignment;
-	private boolean isComment;
-	private boolean isArrayDeclaration;
-	
+	private CodeType type;
+	private enum CodeType {
+		COMMENT, ASSIGNMENT, ARRAY_DECLARATION, STRING, DEFAULT, NOTHING
+	}
+
 	public LineNode(String input) {
 		parse(input);
 	}
-	
+
 	public LineNode(int l, String c, LineNode p, LineNode n) {
 		lineNumber = l;
 		code = c;
 		prev = p;
 		next = n;
 	}
-	
+
 	private void parse(String input) {
 		//go through characters until find a number
 		//once find num, go until find a space
@@ -30,8 +31,8 @@ public class LineNode {
 		char[] arr = input.toCharArray();
 		int lnStart = 0;
 		int lnEnd = 0;
-		
-		
+
+
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] == ' ') {
 				lnEnd = i;
@@ -39,51 +40,51 @@ public class LineNode {
 			}
 			lnEnd = input.length();
 		}
-		
+
 		lineNumber = Integer.parseInt(input.substring(lnStart, lnEnd));		
 		if (input.length() > new Integer(input.substring(lnStart, lnEnd)).toString().length()) {
 			code = input.substring(lnEnd).toUpperCase();
 			if (code.length() >= 4) {
-				isAssignment = code.contains("LET");
-				isComment = !isAssignment && code.contains("REM");
+				if (code.contains("LET")) {
+					type = CodeType.ASSIGNMENT;
+				}
+				if (code.contains("REM")) {
+					type = CodeType.COMMENT;
+				}
 			}
 			else {
-				isAssignment = false;
-				isComment = false;
-			}
-			
-			if (code.contains("DIM")) {
-				checkNameValidity(code.substring(5));
+				type = CodeType.DEFAULT;
 			}
 		}
 		else {
 			code = "";
+			type = CodeType.NOTHING;
 		}
 		next = null;
 		prev = null;
 	}
-	
+
 	/* 
 	Valid names:
 	A, AB, ABC, etc...
 	A1, A2, A3, etc...
-	
+
 	if a string, must have '$'
-	*/
-	private boolean checkNameValidity(String in) {
-		int parenthesesStart = 1;
-		while (in.charAt(parenthesesStart) != '(') {
-			parenthesesStart++;
+	 */
+	private boolean checkType(String in) {
+		switch (type) {
+			case COMMENT:
+				break;
+			case ASSIGNMENT:
+				break;
+			case ARRAY_DECLARATION:
+				break;
+			case STRING:
+				break;
+			default:
+				break;
 		}
-		
-		for (int i = 0; i < parenthesesStart; i++) {
-			if (isArrayDeclaration) {
-				if (Character.isLetter(in.charAt(i)) && i == 0) {
-					
-				}
-			}
-		}
-		
+
 		return true;
 	}
 
@@ -98,11 +99,14 @@ public class LineNode {
 	public LineNode getNext() {
 		return next;
 	}
-	
+
 	public LineNode getPrevious() {
 		return prev;
 	}
-	
+
+	public CodeType getCodeType() {
+		return type;
+	}
 
 	public void setLineNumber(int ln) {
 		lineNumber = ln;
@@ -115,31 +119,19 @@ public class LineNode {
 	public void setNext(LineNode n) {
 		next = n;
 	}
-	
+
 	public void setPrevious(LineNode p) {
 		prev = p;
 	}
-	
+
 	public boolean hasNext() {
 		return next != null;
 	}
-	
+
 	public boolean hasPrevious() {
 		return prev != null;
 	}
-	
-	public boolean isAssignment() {
-		return isAssignment;
-	}
 
-	public boolean isComment() {
-		return isComment;
-	}
-	
-	public boolean isArrayDeclaration() {
-		return isArrayDeclaration;
-	}
-	
 	@Override
 	public String toString() {
 		return lineNumber + " " + code;
